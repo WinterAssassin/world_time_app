632f48213e748d90c9dd0885eed9717cee8240bb
Mark Joseph Cabrera <mjcabrera1996@gmail.com>
2023-03-22 07:53:23 +0800

    Create world time class and pull data from world time API

---
+++ world_time_app/lib/services/world_time.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location;
+  late String time;
+  String flag;
+  String url;
+
+  WorldTime ({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+
+    var address =  Uri.https('worldtimeapi.org', '/api/timezone/$url');
+
+    Response response = await get(address);
+    Map data = jsonDecode(response.body);
+
+  //   if (response.statusCode == 200) {
+  //   Map data = jsonDecode(response.body);
+  //   print(data);
+  // } else {
+  //   print('Request failed with status: ${response.statusCode}.');
+  // }
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    // print(now);
+    time = now.toString();
+
+}
+
+}
